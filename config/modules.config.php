c42d2caf35e40a6f5bd63bff8878eb794f0e6695
Romel Niño Paano <rpaano02@gmail.com>
2021-09-21 10:43:31 +0800

    Initial commit

---
+++ config/modules.config.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * List of enabled modules for this application.
+ *
+ * This should be an array of module namespaces used in the application.
+ */
+return [
+    'Laminas\Db',
+    'Laminas\Cache',
+    'Laminas\Di',
+    'Laminas\Mvc\Middleware',
+    'Laminas\Mvc\Plugin\FilePrg',
+    'Laminas\Mvc\Plugin\FlashMessenger',
+    'Laminas\Mvc\Plugin\Identity',
+    'Laminas\Mvc\Plugin\Prg',
+    'Laminas\Session',
+    'Laminas\Mvc\I18n',
+    'Laminas\Log',
+    'Laminas\Form',
+    'Laminas\Hydrator',
+    'Laminas\InputFilter',
+    'Laminas\Filter',
+    'Laminas\I18n',
+    'Laminas\Router',
+    'Laminas\Validator',
+    'Laminas\DeveloperTools',
+    'Laminas\Diactoros',
+    'Application',
+];
